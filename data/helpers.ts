a02442f91b5185fdfd12fa4d7ba17ef3a95331d0
OchirovAlex <aocirov098@gmail.com>
2024-04-15 20:24:49 -0700

    pseudoInit

--- data/helpers.ts
+++ data/helpers.ts
@@ -23,30 +23,48 @@ export async function signUp(user: string | object | undefined): Promise<any> {
 }
 
 
-// export const randomTour:Tour = {
-//   name: faker.lorem.words(3),
-//   duration: faker.number.int({min:10, max:40}),
-//   description: faker.lorem.words(5),
-//   maxGroupSize: number,
-//   summary: string,
-//   difficulty: string,
-//   price: number,
-//   rating: number,
-//   imageCover: string,
-//   ratingsAverage: number,
-//   guides: object,
-//   startDates: [x:string],
-//   location: {
-//     latitude: number,
-//     longitude: number,
-//     description: string,
-//     address: string,
-//   },
-//   startLocation: {
-//     type: string,
-//     coordinates: number,
-//   }
-// }
+const randomTourName = (charCount: number): string => {
+  const text = faker.lorem.text();
+  let limitedText = text.slice(0, charCount);
+  limitedText = limitedText.slice(0, limitedText.lastIndexOf(' '));
+  return limitedText;
+};
+
+user.aggregate([{ $sample: { size: 1 } }])
+  .then(users => {
+    const randomUser = users[0];
+    
+    const guide = new Guide({
+      guides: [randomUser._id]
+    });
+    
+    return guide.save();
+  })
+
+export const randomTour:Tour = {
+  name: randomTourName(40),
+  duration: faker.number.int({min:10, max:40}),
+  description: faker.lorem.words(5),
+  maxGroupSize: faker.number.int({min: 10, max: 30}),
+  summary: faker.lorem.words(10),
+  difficulty: ,
+  price: faker.number.int(),
+  rating: faker.number.float({min:1.0, max:5.0}),
+  imageCover: `tour-${faker.number.int({min:1,max:9})}-cover.jpg`,
+  ratingsAverage: faker.number.float({min:1.0, max:5.0}),
+  guides: object,
+  startDates: [x:string],
+  location: {
+    latitude: number,
+    longitude: number,
+    description: string,
+    address: string,
+  },
+  startLocation: {
+    type: string,
+    coordinates: number,
+  }
+}
 
 
 export function upload(files: string[]): Promise<any> {
